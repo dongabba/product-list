6acc20c41517803c61825a1959ddd0ae82a39785
Alexander Zhaleiko <azhaleiko@gmail.com>
2020-04-16 11:26:57 +0300

    add validation for product list

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,24 @@
     section
       .container
         h1.ui-title-1 Home
-        .product-add
-          input(
-              type="text"
-              placeholder="Введите название продукта ..."
-              v-model="productName"
-              @keyup.enter="addProduct"
-          )
-          .button.button--round.button-default(
-              @click="addProduct"
-              ) Добавить
+        form(@submit.prevent="addProduct")
+          .add-products-form(:class="{ 'errorInput': $v.productName.$error }")
+            .product-add
+              input(
+                  type="text"
+                  placeholder="Введите название продукта ..."
+                  v-model="productName"
+                  @keyup.enter="addProduct"
+                  :class="{ errorInput: $v.productName.$error }"
+                  @change="$v.productName.$touch()"
+              )
+              button.button.button--round.button-default(type="submit" :disabled="submitStatus === 'PENDING'") Добавить
+            .add-products-info
+              .messages-error
+               .error(v-if="!$v.productName.required") Поле должно быть заполнено
+              .messages-info
+                p.typo__p(v-if="submitStatus === 'OK'") Продукт добавлен
+                p.typo__p(v-if="submitStatus === 'PENDING'") Добавляем...
     section
       .container
         .product-list
@@ -30,6 +38,7 @@
             ) {{ product.productName }}
 </template>
 <script>
+import { required } from 'vuelidate/lib/validators'
 export default {
   computed: {
     products () {
@@ -38,36 +47,59 @@ export default {
   },
   data () {
     return {
-      productName: ''
+      productName: '',
+      submitStatus: null
     }
   },
   methods: {
     addProduct () {
-      if (this.productName === '') {
-        return
+      console.log('submit!')
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR'
+      } else {
+        this.submitStatus = 'PENDING'
+        setTimeout(() => {
+          this.submitStatus = 'OK'
+        }, 500)
+        const newProduct = {
+          productName: this.productName,
+          isVisible: false
+        }
+        this.$store.dispatch('addProduct', newProduct)
+        this.productName = ''
+        this.$v.$reset()
       }
-      const newProduct = {
-        productName: this.productName,
-        isVisible: false
-      }
-
-      this.$store.dispatch('addProduct', newProduct)
-
-      // Reset
-      this.productName = ''
+    }
+  },
+  validations: {
+    productName: {
+      required
     }
   }
 }
 </script>
 <style lang="stylus" scoped>
+.add-products-form
+ .error
+    display none
+    margin-bottom 8px
+    font-size 13.4px
+    color #fc5c65
+  &.errorInput
+    .error
+      display block
+
 .product-add
   display flex
   justify-content space-between
   align-items center
+  margin-bottom0
   input
-     margin-bottom 0
-     height 42px
-     margin-right 10px
+    &.errorInput
+      border-color #fc5c65
+    height 42px
+    margin-right 10px
 
 .product-list__item
   display flex
